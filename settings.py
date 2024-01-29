de919d5f4b6e1d9f9484a3e35b0d341dd10e25c2
Nuworklo <cnuworklo@deloitte.com.gh>
2024-01-29 11:43:42 +0000

    Updated settings.py

--- settings.py
+++ settings.py
@@ -8,10 +8,10 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 QUEUES = ["emails", "default"]
 
 if REDIS_URL:
-    connection_pool = ConnectionPool.from_url(REDIS_URL)
-    redis_connection = Redis(connection_pool=connection_pool)
-else:
-    print("Warning: REDIS_URL is not set. Using default Redis connection.")
-    connection_pool = ConnectionPool.from_url('redis://localhost:6379/0')
-    redis_connection = Redis(connection_pool=connection_pool)
-    
+    try:
+        connection = Redis.from_url(REDIS_URL)
+    except ConnectionError as e:
+        print(f"Error connecting to Redia: {e}")
+    else:
+        print("REDIS_URL is not set.")
+            
