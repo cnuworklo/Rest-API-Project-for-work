78552285181de91a2110f7d3f3ad6a74db3517bb
Nuworklo <cnuworklo@deloitte.com.gh>
2024-01-29 11:24:44 +0000

    Updated settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,17 @@
 import os
 from dotenv import load_dotenv
+from redis import ConnectionPool, Redis
 
 load_dotenv()
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 QUEUES = ["emails", "default"]
+
+if REDIS_URL:
+    connection_pool = ConnectionPool.from_url(REDIS_URL)
+    redis_connection = Redis(connection_pool=connection_pool)
+else:
+    print("Warning: REDIS_URL is not set. Using default Redis connection.")
+    connection_pool = ConnectionPool.from_url('redis://localhost:6379/0')
+    redis_connection = Redis(connection_pool=connection_pool)
+    
